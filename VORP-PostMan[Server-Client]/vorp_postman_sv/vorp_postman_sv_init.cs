14935a0fd127811348706a5be1176e0887c7e248
colormeblind23 <colormeblind22@gmail.com>
2020-10-16 02:48:13 -0400

    API to 2.0
    Converted to 2.0 API

--- VORP-PostMan[Server-Client]/vorp_postman_sv/vorp_postman_sv_init.cs
+++ VORP-PostMan[Server-Client]/vorp_postman_sv/vorp_postman_sv_init.cs
@@ -9,18 +9,27 @@ namespace vorp_postman_sv
 {
     class vorp_postman_sv_init : BaseScript
     {
+        public static dynamic VorpCore; // Save one time and share with all class
+
         public vorp_postman_sv_init()
         {
+            //GetCore CallBack
+            TriggerEvent("getCore", new Action<dynamic>((core) =>
+            {
+                VorpCore = core; //Save Core Import
+            }));
             EventHandlers["vorp_postman:receiveRewards"] += new Action<Player, int, int>(addRewards);
         }
 
         private void addRewards([FromSource]Player source, int postOffice, int deliverLocation)
         {
+            dynamic User = VorpCore.getUser(source); // Return User with functions and all characters
+            dynamic Character = User.getUsedCharacter; // Return character selected by user
             int _source = int.Parse(source.Handle);
             double moneyReward = GetRandomNumber(LoadConfig.Config["PostOffices"][postOffice]["JobZones"][deliverLocation]["Money"][0].ToObject<double>(), LoadConfig.Config["PostOffices"][postOffice]["JobZones"][deliverLocation]["Money"][1].ToObject<double>());
             int xpReward = LoadConfig.Config["PostOffices"][postOffice]["JobZones"][deliverLocation]["Xp"].ToObject<int>();
-            TriggerEvent("vorp:addMoney", _source, 0, moneyReward);
-            TriggerEvent("vorp:addXp", _source, xpReward);
+            Character.addCurrency(0, moneyReward);
+            Character.addXp(xpReward);
             source.TriggerEvent("vorp:TipRight", string.Format(LoadConfig.Langs["YouReceive"], moneyReward, xpReward));
         }
 
